2f5943cb2b6aeb224555b7bfc33fab092d53f669
Mohammad Alsharif <alsharifms@gmail.com>
2020-12-20 21:47:10 +0300

    small change

--- __test__/handleSubmit.test.js
+++ __test__/handleSubmit.test.js
@@ -1,6 +1,7 @@
 import { apiRun } from '../src/client/js/formHandler'
 global.fetch = require('node-fetch')
 
+// Localhost 8081 needs to be running for the test to pass
 test('WORK API', async () => {
     const input = 'www.example.com';
     return apiRun(input).then(apiData => {
